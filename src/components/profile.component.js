f25bdb970c7ada27905d7b1afd02b0379f67ebb8
FrancaTM <francatm@outlook.com>
2020-12-20 18:12:50 -0300

    fix component bug

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
 import AuthService from "../services/auth.service";
 
@@ -7,39 +8,63 @@ export default class Profile extends Component {
     super(props);
 
     this.state = {
-      currentUser: AuthService.getCurrentUser,
+      redirect: null,
+      userReady: false,
+      currentUser: { username: "" },
+      // currentUser: AuthService.getCurrentUser,
     };
   }
 
+  componentDidMount() {
+    const currentUser = AuthService.getCurrentUser();
+
+    if (!currentUser) {
+      this.setState({ redirect: "/home" });
+    }
+    this.setState({ currentUser: currentUser, userReady: true });
+  }
+
   render() {
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />;
+    }
+
     const { currentUser } = this.state;
 
     return (
       <div className="container">
-        <header className="jumbotron">
-          <h3>
-            <strong>{currentUser.username}</strong> Profile
-          </h3>
-        </header>
+        {this.state.userReady ? (
+          <div>
+            <header className="jumbotron">
+              <h3>
+                <strong>{currentUser.username}</strong> Profile
+              </h3>
+            </header>
 
-        <p>
-          <strong>Token:</strong>
-          {currentUser.accessToken.substring(0, 20)} ...{" "}
-          {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-        </p>
-        <p>
-          <strong>Id:</strong>
-          {currentUser.id}
-        </p>
-        <p>
-          <strong>Email:</strong>
-          {currentUser.email}
-        </p>
-        <strong>Authorities:</strong>
-        <ul>
-          {currentUser.roles &&
-            currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-        </ul>
+            <p>
+              <strong>Token:</strong>{" "}
+              {currentUser.accessToken.substring(0, 20)} ...{" "}
+              {currentUser.accessToken.substr(
+                currentUser.accessToken.length - 20
+              )}
+            </p>
+            <p>
+              <strong>Id:</strong>{" "}
+              {currentUser.id}
+            </p>
+            <p>
+              <strong>Email:</strong>{" "}
+              {currentUser.email}
+            </p>
+            <strong>Authorities:</strong>
+            <ul>
+              {currentUser.roles &&
+                currentUser.roles.map((role, index) => (
+                  <li key={index}>{role}</li>
+                ))}
+            </ul>
+          </div>
+        ) : null}
       </div>
     );
   }
